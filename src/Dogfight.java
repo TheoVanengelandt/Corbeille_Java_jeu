f7e5b01ec1fd4b046be3e910609081b682025cd7
TheoVanengelandt <theo.vanengelandt@viacesi.fr>
2017-06-02 11:50:51 +0200

    Phase 3

--- src/Dogfight.java
+++ src/Dogfight.java
@@ -2,7 +2,13 @@
 public class Dogfight {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		final DogfightModel dogfightModel = new DogfightModel();
+		final DogfightController dogfightController = new DogfightController(dogfightModel);
+		final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel,
+		dogfightModel);
+		dogfightController.setViewSystem(dogfightView);
+		dogfightController.play();
 
 	}
 
