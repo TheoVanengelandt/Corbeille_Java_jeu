721ef70d12f0318b0819d99d9f1b15af8b057823
tdeclety <compteapubs@gmail.com>
2017-06-06 14:41:56 +0200

    td_4(la bonne)

--- src/jpu2016/dogfight/modele/DogfightModel.java
+++ src/jpu2016/dogfight/modele/DogfightModel.java
@@ -1,12 +1,30 @@
 package jpu2016.dogfight.modele;
 
 
-public class DogfightModel implements IDogfightModel {
+public class DogfightModel implements IDogfightModel extends observable{
 	
 	public DogfightModel() {
 	}
 	public IArea getArea() {
 		return null;
+	}
+	public void buildArea(Dimension dimension){
+		
+	}
+	public void addMobile(IMobile Mobile){
+		
+	}
+	public void removeMobile(IMobile Mobile){
+		
+	}
+	public ArrayList<IMobile> getMobiles(){
+		
+	}
+	public IMobile getMobileByPlayer(int player){
+		
+	}
+	public void setMobilesHavesMoved(){
+		
 	}
 
 }
