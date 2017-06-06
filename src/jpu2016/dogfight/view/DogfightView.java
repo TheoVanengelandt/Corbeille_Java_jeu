2a0a3044064f61e0cf3151b97df1ae8634e67bee
julien-juncker <julien.juncker@viacesi.fr>
2017-06-06 10:07:46 +0200

    DogfightView changed

--- src/jpu2016/dogfight/view/DogfightView.java
+++ src/jpu2016/dogfight/view/DogfightView.java
@@ -1,6 +1,25 @@
 package jpu2016.dogfight.view;
 
+import java.util.Observable;
+
+import jpu2016.dogfight.controller.IOrderPerformer;
+import jpu2016.dogfight.modele.IDogfightModel;
+
 public class DogfightView {
-
-
+	
+	public DogfightView(IOrderPerformer orderPerformer,IDogfightModel dogfightModel, Observable observable) {
+		
+	}
+	
+	private void run() {
+		
+	}
+	
+	public String displayMessage (String message) {
+		return message ;
+	}
+	
+	public void closeAll() {
+		
+	}
 }
