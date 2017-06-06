49cf23b7afb180219e7a510901a5fa6e0759e91f
TheoVanengelandt <theo.vanengelandt@viacesi.fr>
2017-06-06 09:44:38 +0200

    Changement 1.1

--- src/Dogfight.java
+++ src/Dogfight.java
@@ -1,5 +1,6 @@
 import jpu2016.dogfight.controller.DogfightController;
 import jpu2016.dogfight.modele.DogfightModel;
+import jpu2016.dogfight.modele.IDogfightModel;
 import jpu2016.dogfight.view.DogfightView;
 
 public class Dogfight {
@@ -7,7 +8,7 @@ public class Dogfight {
 	public static void main(String[] args) {
 		
 		final DogfightModel dogfightModel = new DogfightModel();
-		final DogfightController dogfightController = new DogfightController(dogfightModel);
+		final DogfightController dogfightController = new DogfightController((IDogfightModel) dogfightModel);
 		final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel);
 		dogfightController.setViewSystem(dogfightView);
 		dogfightController.play();
